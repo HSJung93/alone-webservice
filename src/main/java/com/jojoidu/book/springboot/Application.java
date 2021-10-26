edef201db6dcfffa347a3baa1afc968492be28e0
Hoesung <jrps2212@gmail.com>
2021-10-26 16:07:15 +0900

    nginx 이전까지

--- src/main/java/com/jojoidu/book/springboot/Application.java
+++ src/main/java/com/jojoidu/book/springboot/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-// nginx 설정 수정
+// nginx 없이 다시
 @EnableJpaAuditing
 // 이 어노테이션부터 설정을 읽어가기 때문에 프로젝트의 최상단에 있어야 한다.
 // 스프링 Bean 읽기와 생성 등 자동 설정을 해준다.
